c9b80e838959b652fe85e92717622b3059b0333b
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-16 10:40:41 -0600

    built library and schedule components and put them on the grid

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Library from './schedule/library/library';
+import Schedule from './schedule/schedule'
+
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Course Scheduler</h1>
+        <Library/>
+        <Schedule/>
       </div>
     );
   }
